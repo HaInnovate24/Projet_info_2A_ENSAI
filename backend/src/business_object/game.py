048338218d7165cfb33685fe2e1eab31103d664c
id3159 <sidy-mohamed-salim.diallo@eleve.ensai.fr>
2026-09-04 09:13:22 +0000

    finish to add base coin flip 2.2

---
+++ backend/src/business_object/game.py
@@ -0,0 +1,26 @@
+from player import Player
+
+
+class Game:
+    def __init__(
+        self,
+        player1: Player,
+        player2: Player,
+        game_mode,
+        winner: Player | None,
+        description: str | None,
+        timestamp,
+    ):
+        self.id_game = None
+        self.player1 = player1
+        self.player2 = player2
+        self.game_mode = game_mode
+        self.winner = winner
+        self.description = description
+        self.timestamp = timestamp
+
+    def __str__(self):
+        if self.winner:
+            return f"{self.game_mode} entre {self.player1.username} et {self.player2.username}. Gagnante: {self.winner.username} "
+        else:
+            return f"{self.game_mode} entre {self.player1.username} et {self.player2.username}. Pas encore de gagnante "
---
+++ backend/src/business_object/game_mode /dice_mode.py
@@ -0,0 +1,20 @@
+import secrets
+
+from business_object.game_mode.game_mode import GameMode
+
+from business_object.game import Game
+from business_object.player import Player
+
+
+class DiceMode(GameMode):
+    def play(self, p1: Player, p2: Player) -> Game:
+        d1 = secrets.choice(range(1, 7))
+        d2 = secrets.choice(range(1, 7))
+        if d1 > d2:
+            winner = p1
+        elif d1 < d2:
+            winner = p2
+        else:
+            winner = None
+
+        return winner
